0056ab5952ab1b90cb839b3511df073aa332a70f
0-LG-0 <luigigjurashaj@gmail.com>
2026-01-30 18:48:41 -0500

    Exercise 1

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => console.log("this app is running on port 3000"))
+
+app.get('/greetings/:username', (req, res) => {
+    res.send(`Hello there, ${req.params.username}.`)
+})
