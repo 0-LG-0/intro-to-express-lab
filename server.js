6bfb7b19f946bcd41d4dc4f1983d1dc60611f7d4
0-LG-0 <luigigjurashaj@gmail.com>
2026-01-30 18:59:54 -0500

    Exercise 2

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ app.listen(3000, () => console.log("this app is running on port 3000"))
 app.get('/greetings/:username', (req, res) => {
     res.send(`Hello there, ${req.params.username}.`)
 })
+
+app.get('/roll/:number', (req, res) => {
+    if (Number.isInteger(parseInt(req.params.number))) {
+        res.send(`<h1>You rolled a ${Math.floor(Math.random() * req.params.number)}</h1>`)
+    }else {
+        res.send(`You must specify a number, ${req.params.number} is not a number.`)
+    }
+})
+
